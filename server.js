bf70c11e3e847bee9ac140268ff52c13d5fd3be4
DaMegaPic <damegapic@gmail.com>
2025-04-06 20:50:50 -0400

    api and styles

--- server.js
+++ server.js
@@ -1,14 +1,44 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
+
 app.use(express.static("public"));
 app.use(express.json());
 app.use(cors());
 
-app.get("/",(req, res)=>{
-    res.sendFile(__dirname+"/index.html");
+// Temporary in-memory "database"
+const reviews = [
+  {
+    id: 1,
+    title: "Great Work!",
+    rating: "5",
+    code: "ABC123",
+    description: "They did a fantastic job with my kitchen.",
+    image: "kitchen.jpg",
+    date: "1/1/00",
+    reviewer: "Colson"
+  },
+  {
+    id: 2,
+    title: "Very Professional",
+    rating: "3",
+    code: "XYZ789",
+    description: "Professional and friendly team.",
+    image: "bathroom.jpg",
+    date: "1/1/00",
+    reviewer: "Colson"
+  }
+];
+
+// API route to get reviews
+app.get("/api/reviews", (req, res) => {
+  res.json(reviews);
 });
 
-app.listen(3001, ()=>{
-    console.log("I'm listening")
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
+
+app.listen(3001, () => {
+  console.log("Server is listening on port 3001");
 });
